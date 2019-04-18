cb32e782e3b825f84b42ff4d1dbf2307a324ecf2
ugrcoban <49412652+ugrcoban@users.noreply.github.com>
2019-04-18 15:07:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?PHP
 
+	mysqli_report(MYSQLI_REPORT_STRICT); 
 	
 	echo "Welcome! You can Use PHP & NGINX";
 	echo "<br><br>Phpmyadmin : {local_ip}:8080 (username : root // password: root_password)";
@@ -7,17 +8,18 @@
 	
 	// development.env
 	$host = 'mysql';#docker-compose.yml defined:hostname
-	$db = getenv('sys_db');
+	$db = getenv('MYSQL_DATABASE');
 	$user = getenv('MYSQL_USER');
 	$pass = getenv('MYSQL_PASSWORD');
- 
+
+			
 	try {
 		$conn = mysqli_connect($host, $user, $pass);
 	} catch(mysqli_sql_exception  $e) {
 		if($e->getCode()==2002)#DB initilazing for docker
-			die("Please waiting a few seconds for mysql initilazing.. And refresh again.");
+			die("<br><br>Please waiting a few seconds for mysql initilazing.. And refresh again.");
 		else
-			exit('Connection failed: '.mysqli_connect_error().PHP_EOL);
+			exit('<br><br>Connection failed: '.mysqli_connect_error().PHP_EOL);
 	}
  
 	echo '<br><br>MYSQL : Successful database connection!';
